5313e585371a0d4f6d8ebc95eeb7c1bcdcd9ef47
clumsyyyy <71161031+clumsyyyy@users.noreply.github.com>
2021-04-08 07:33:41 +0700

    delete bbrp fungsi buat testing

--- hashingtest.py
+++ hashingtest.py
@@ -1,17 +1,4 @@
-def hashing(str):
-
-
-    p = 56
-    m = 1e9 + 9
-    pangkat = 1
-    val = 0
-
-    for i in range(len(str)):
-        val = (val + (ord(str[i]) - ord('a') + 1) * pangkat) % m
-        pangkat *= p % m
-    return int(val)
-
-def hashing2(str):
+def hashing2(str): #buat yang hashing, pake polynomial rolling function
     p = 56
     m = 1e9 + 9
     pangkat = 1
@@ -23,6 +10,3 @@ def hashing2(str):
     return(int(val % m))
 
 print(hashing2(input()))
-
-for i in range(95, -1, -1):
-     print("{} shell".format(i))
